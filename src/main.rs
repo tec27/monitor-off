67b73f9b8f97a5c1c5dc8dd094a8a2120c747ebb
Travis Collins <travis@tec27.com>
2024-02-22 16:40:40 -0800

    Wait a small amount of time before sending the monitor power message to avoid the triggering event re-waking the monitor. Also destroy the created window properly (doesn't really matter but probably nice to do).

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{thread, time::Duration};
+
 use windows::{
     core::{s, Result},
     Win32::{
         Foundation::{LPARAM, WPARAM},
         UI::WindowsAndMessaging::{
-            CreateWindowExA, SendMessageA, SC_MONITORPOWER, WINDOW_EX_STYLE, WINDOW_STYLE,
-            WM_SYSCOMMAND,
+            CreateWindowExA, DestroyWindow, SendMessageA, SC_MONITORPOWER, WINDOW_EX_STYLE,
+            WINDOW_STYLE, WM_SYSCOMMAND,
         },
     },
 };
 
 fn main() -> Result<()> {
     unsafe {
+        // Without this it the keyup/mouseup/etc. seem to arrive after turning the monitor off and
+        // turn it back on again.
+        thread::sleep(Duration::from_millis(500));
+
         let handle = CreateWindowExA(
             WINDOW_EX_STYLE::default(),
             s!("Button"),
@@ -34,6 +40,8 @@ fn main() -> Result<()> {
             WPARAM(SC_MONITORPOWER as usize),
             LPARAM(2), /* MONITOR_OFF */
         );
+
+        let _ = DestroyWindow(handle);
     }
 
     Ok(())
